6e560711f6f50b872ec3240660812cb290d7d3ec
Dilum B <s220459698@deakin.edu.au>
2023-07-29 15:30:16 +1000

    SIT725 - Express Web Server with adding numbers

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let app = express();
-let portnumber = process.env.port || 3000;
+let port = process.env.port || 3000;
 
 app.use(express.static(__dirname+'/'));
 
@@ -8,6 +8,20 @@ app.get('/', (req,res)=>{
     res.render('index.html');
 });
 
-app.listen(portnumber, ()=>{
+app.get('/addTwoNumber',(req,res)=>{
+    //grab values from url paramter
+    let num1 = req.query.number1;
+    let num2 = req.query.number2;
+
+    let sum = parseInt(num1) + parseInt(num2);
+    //do math
+    let obj = {statusCode:200, message:'success',data:sum}
+
+    res.json(obj);
+
+    
+});
+
+app.listen(port, ()=>{
     console.log("Server Started");
 }); // this is the logic that will fired upon server start
